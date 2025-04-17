e5313d2c8f9eb1db572f07ff4b8e49138a050835
ragul0427 <121870806+ragul0427@users.noreply.github.com>
2025-04-17 18:01:45 +0530

    Update

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectTOMongoDb = async () => {
     try {
-        const uri = process.env.MONGO_URL; // Use an environment variable for your URI
+        const uri = process.env.MONGO_URL; 
         await mongoose.connect(uri);
         console.log("Connected To MongoDb");
     }
